239bf947496ff10b5b87c7b2ab80a2af1d3a7d89
antony-jr <antonyjr@protonmail.com>
2020-05-21 19:49:59 +0530

    Upgrade demo

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-import threading
 import cv2
 import tempfile
 import os
-from flask import Flask,render_template,send_file,redirect,request
+import uuid
+from flask import Flask,jsonify,render_template,send_file,redirect,request
 from werkzeug.utils import secure_filename
 from OBR import SegmentationEngine,BrailleClassifier,BrailleImage
 
+
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 tempdir = tempfile.TemporaryDirectory()
-lock = threading.Lock()
-image_path = 'samples/sample1.png'
-classifier = BrailleClassifier()
 
 app = Flask("Optical Braille Recognition Demo")
 app.config['UPLOAD_FOLDER'] = tempdir.name
@@ -19,66 +17,59 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.after_request
-def add_header(response):
-    response.headers['Cache-Control'] = 'no-cache, no-store'
-    response.headers['Pragma'] = 'no-cache'
-    return response
-
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/current_image')
-def current_image():
-    global lock
-    lock.acquire()
-    global image_path
-    image = image_path
-    lock.release()
-    return send_file(image, mimetype='image/png')
+@app.route('/favicon.ico')
+def fav():
+    return send_file('favicon.ico', mimetype='image/ico')
 
-@app.route('/processed_image')
-def proc_image():
+@app.route('/coverimage')
+def cover_image():
+    return send_file('samples/sample1.png', mimetype='image/png')
+
+@app.route('/procimage/<string:img_id>')
+def proc_image(img_id):
     global tempdir
-    return send_file('{}/proc.png'.format(tempdir.name), mimetype='image/png')
+    image = '{}/{}-proc.png'.format(tempdir.name, secure_filename(img_id))
+    if os.path.exists(image) and os.path.isfile(image):
+        r = send_file(image, mimetype='image/png')
+        os.unlink(image)
+        return r
+    return redirect('/coverimage')
 
-@app.route('/digest')
-def digest():
-    global lock
-    lock.acquire()
-    global image_path
-    global classifier
-    global tempdir
-    img = BrailleImage(image_path)
-    classifier.clear()
-    for letter in SegmentationEngine(image=img):
-        letter.mark()
-        classifier.push(letter)
-    cv2.imwrite('{}/proc.png'.format(tempdir.name), img.get_final_image())
-    lock.release()
-    return classifier.digest()
-
-@app.route('/upload', methods=['POST'])
+@app.route('/digest', methods=['POST'])
 def upload():
     # check if the post request has the file part
     if 'file' not in request.files:
-        return redirect('/')
+        return jsonify({"error" : True, "message" : "file not in request"})
     file = request.files['file']
     # if user does not select file, browser also
     # submit an empty part without filename
     if file.filename == '':
-        return redirect('/')
+        return jsonify({"error": True, "message" : "empty filename"})
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'uploaded'))
-        global lock
-        lock.acquire()
-        global image_path
+        filename = ''.join(str(uuid.uuid4()).split('-'))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         global tempdir
-        image_path = '{}/uploaded'.format(tempdir.name)
-        lock.release()
-        return redirect('/')
+        
+        image_path = '{}/{}'.format(tempdir.name,filename)
+        classifier = BrailleClassifier()
+        img = BrailleImage(image_path)
+        for letter in SegmentationEngine(image=img):
+            letter.mark()
+            classifier.push(letter)
+        cv2.imwrite('{}/{}-proc.png'.format(tempdir.name,filename), img.get_final_image())
+        os.unlink(image_path)
+
+        r = {
+                "error": False,
+                "message": "Processed and Digested successfully",
+                "img_id" : filename,
+                "digest" : classifier.digest()
+        }
+        return jsonify(r)
 
 if __name__ == "__main__":
     app.run()
